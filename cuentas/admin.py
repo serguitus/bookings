bb71a78cbb2181f069376657739925d65088ce81
serguitus <serguitus@gmail.com>
2017-04-06 15:23:25 -0400

    customizing Caja admin model

--- cuentas/admin.py
+++ cuentas/admin.py
@@ -4,5 +4,10 @@ from django.contrib import admin
 
 from models import Caja, Transaction
 
-admin.site.register(Caja)
+@admin.register(Caja)
+class CajaAdmin(admin.ModelAdmin):
+    """ an admin interface for the Cajas """
+    list_display = ('__str__', 'ammount')
+
+
 admin.site.register(Transaction)
