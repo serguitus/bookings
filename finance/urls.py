42fd66547538e0bf83f11bcd2903caa15070c6ba
serguitus <serguitus@gmail.com>
2018-05-17 23:22:56 -0400

    working on first form

--- finance/urls.py
+++ finance/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 
 from . import views
 
@@ -8,5 +8,9 @@ urlpatterns = [
     url(r'^loan/$', views.loan, name='loan'),
     url(r'^loandevolution/$', views.loandevolution, name='loandevolution'),
     url(r'^loanmatch/$', views.loanmatch, name='loanmatch'),
-    url(r'^documents/$', views.documents, name='documents'),
+    url(r'^documents/',
+        include([
+            url(r'^$', views.documents_list, name='documents_list'),
+            url(r'^(?P<pk>\d+)/', views.document_edit, name='document_edit'),
+        ])),
 ]
--- finance/views.py
+++ finance/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 
 from finance.models import FinantialDocument
+from finance.forms import DepositForm
 
 
 # Create your views here.
-def documents(request):
+def documents_list(request):
     document_list = FinantialDocument.objects.order_by('-date')[:20]
     context = {
         'document_list': document_list,
@@ -12,21 +13,44 @@ def documents(request):
     return render(request, 'list.html', context)
 
 
+def document_edit(request, pk):
+    """ A view to edit a FinantialDocument"""
+    count = 0
+    if request.method == 'POST':
+        # submitting form
+        deposit_form = DepositForm(request.POST)
+        if deposit_form.is_valid():
+            deposit_form.save()
+            count = 1
+    else:
+        # GET request
+        try:
+            fd = FinantialDocument.objects.get(pk=pk)
+        except FinantialDocument.DoesNotExist:
+            fd = None
+        deposit_form = DepositForm(instance=fd)
+    context = {
+        'count': count,
+        'form': deposit_form
+    }
+    return render(request, 'edit.html', context)
+
+
 def deposit(request):
-    return render('aqui va la respuesta')
+    return render(request, 'aqui va la respuesta')
 
 
 def withdraw(request):
-    return render('aqui va la respuesta')
+    return render(request, 'aqui va la respuesta')
 
 
 def loan(request):
-    return render('aqui va la respuesta')
+    return render(request, 'aqui va la respuesta')
 
 
 def loandevolution(request):
-    return render('aqui va la respuesta')
+    return render(request, 'aqui va la respuesta')
 
 
 def loanmatch(request):
-    return render('aqui va la respuesta')
+    return render(request, 'aqui va la respuesta')
