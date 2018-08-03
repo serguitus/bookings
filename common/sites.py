507eea97fd01c6e86fbb1f6a44f479a0897df930
Daniel Lodos <ipdlx0@gmail.com>
2018-08-02 22:00:07 -0400

    mejoras y template de match

--- common/sites.py
+++ common/sites.py
@@ -1,3 +1,4 @@
+import copy
 import json
 import string
 
@@ -10,9 +11,10 @@ from django.contrib.admin.options import csrf_protect_m, TO_FIELD_VAR, IS_POPUP_
 from django.contrib.admin.sites import AdminSite
 from django.contrib.admin.utils import quote, unquote, get_deleted_objects
 from django.contrib.admin.views.main import ChangeList
+from django.contrib.admin.widgets import RelatedFieldWidgetWrapper
 from django.contrib.auth import get_permission_codename, logout as auth_logout
 from django.core.exceptions import ValidationError, PermissionDenied
-from django.db import router, transaction
+from django.db import router, transaction, models
 from django.forms import fields_for_model
 from django.forms.formsets import all_valid
 from django.http import HttpResponseRedirect
@@ -271,6 +273,14 @@ class CommonSite(AdminSite):
             reverse('%s:login' % self.site_namespace, current_app=self.name)
         )
 
+class CommonRelatedFieldWidgetWrapper(RelatedFieldWidgetWrapper):
+
+    def get_related_url(self, info, action, *args):
+        return reverse(
+            "common:%s_%s_%s" % (info + (action,)),
+            current_app=self.admin_site.name,
+            args=args)
+
 
 class SiteModel(ModelAdmin):
     save_on_top = True
@@ -302,6 +312,67 @@ class SiteModel(ModelAdmin):
     popup_response_template = None
 
     recent_allowed = True
+
+    def related_field_widget_wrapper_class(self):
+        return CommonRelatedFieldWidgetWrapper
+
+    def formfield_for_dbfield(self, db_field, request, **kwargs):
+        """
+        Hook for specifying the form Field instance for a given database Field
+        instance.
+
+        If kwargs are given, they're passed to the form Field's constructor.
+        """
+        # If the field specifies choices, we don't need to look for special
+        # admin widgets - we just need to use a select widget of some kind.
+        if db_field.choices:
+            return self.formfield_for_choice_field(db_field, request, **kwargs)
+
+        # ForeignKey or ManyToManyFields
+        if isinstance(db_field, models.ManyToManyField) or isinstance(db_field, models.ForeignKey):
+            # Combine the field kwargs with any options for formfield_overrides.
+            # Make sure the passed in **kwargs override anything in
+            # formfield_overrides because **kwargs is more specific, and should
+            # always win.
+            if db_field.__class__ in self.formfield_overrides:
+                kwargs = dict(self.formfield_overrides[db_field.__class__], **kwargs)
+
+            # Get the correct formfield.
+            if isinstance(db_field, models.ForeignKey):
+                formfield = self.formfield_for_foreignkey(db_field, request, **kwargs)
+            elif isinstance(db_field, models.ManyToManyField):
+                formfield = self.formfield_for_manytomany(db_field, request, **kwargs)
+
+            # For non-raw_id fields, wrap the widget with a wrapper that adds
+            # extra HTML -- the "add other" interface -- to the end of the
+            # rendered output. formfield can be None if it came from a
+            # OneToOneField with parent_link=True or a M2M intermediary.
+            if formfield and db_field.name not in self.raw_id_fields:
+                related_modeladmin = self.admin_site._registry.get(db_field.remote_field.model)
+                wrapper_kwargs = {}
+                if related_modeladmin:
+                    wrapper_kwargs.update(
+                        can_add_related=related_modeladmin.has_add_permission(request),
+                        can_change_related=related_modeladmin.has_change_permission(request),
+                        can_delete_related=related_modeladmin.has_delete_permission(request),
+                    )
+                widget_wrapper_class = self.related_field_widget_wrapper_class()
+                formfield.widget = widget_wrapper_class(
+                    formfield.widget, db_field.remote_field, self.admin_site, **wrapper_kwargs
+                )
+
+            return formfield
+
+        # If we've got overrides for the formfield defined, use 'em. **kwargs
+        # passed to formfield_for_dbfield override the defaults.
+        for klass in db_field.__class__.mro():
+            if klass in self.formfield_overrides:
+                kwargs = dict(copy.deepcopy(self.formfield_overrides[klass]), **kwargs)
+                return db_field.formfield(**kwargs)
+
+        # For any other type of field, just call its formfield() method.
+        return db_field.formfield(**kwargs)
+
 
     def has_add_permission(self, request):
         return (not self.readonly_model) \
@@ -435,18 +506,19 @@ class SiteModel(ModelAdmin):
         context.update(self.admin_site.get_site_extra_context(request))
         return context
 
-    def recent_link(self, request, model_object, url=None, label=None, icon=None):
+    def recent_link(self, request, model_object):
         """
         for adding recent link for user and url
         """
         try:
             if self.recent_allowed and model_object and model_object.pk:
-                if not url:
-                    url = request.get_full_path()
-                if not label:
-                    label = model_object.__str__()
-                if not icon:
-                    icon = self.model._meta.model_name
+                opts = model_object._meta
+                url = reverse(
+                    self.change_url_format() % (opts.app_label, opts.model_name),
+                    args=(quote(model_object.pk),),
+                    current_app=self.admin_site.name)
+                label = model_object.__str__()
+                icon = self.model._meta.model_name
                 # TODO add url from request is useless
                 #if self.is_add_url(url):
                 #    url = reverse(self.change_url_format() % (
