8dd55f82c049116370dd2472658a9632a2d28fde
serguitus <serguitus@gmail.com>
2018-09-06 19:02:21 -0400

    starting with bookings list

--- booking/common_site.py
+++ booking/common_site.py
@@ -1,9 +1,11 @@
 """
 bookings site models
 """
+from django.conf.urls import url
+from django.shortcuts import render
 
-from booking.models import Booking
 from common.sites import SiteModel
+from booking.models import Booking
 from reservas.admin import bookings_site
 
 MENU_LABEL_BOOKINGS = 'Reservas'
@@ -15,6 +17,23 @@ class BookingSiteModel(SiteModel):
     list_filter = ['agency', 'status']
     search_fields = ['reference']
     ordering = ['date_from', 'reference']
+
+    def get_urls(self):
+        urls = super(BookingSiteModel, self).get_urls()
+        other_urls = [
+            url(r'^bookinglist/$', self.booking_list),
+        ]
+        return other_urls + urls
+
+    def booking_list(self, request):
+        """ a list of bookings with their services """
+        context = {}
+        context.update(self.get_model_extra_context(request))
+        bookings = Booking.objects.all()
+        context.update({
+            "bookings": bookings,
+        })
+        return render(request, 'booking/booking_list.html', context)
 
 
 bookings_site.register(Booking, BookingSiteModel)
--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def booking_list(request, instance):
+    """ a list of bookings with their services """
+    context = {}
+    context.update(instance.get_model_extra_context(request))
+    return render(request, 'booking/booking_list.html', context)
