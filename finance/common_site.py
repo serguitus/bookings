04a3e8021ffe9a2015719796c49a2b19d39c7569
Daniel Lodos <ipdlx0@gmail.com>
2019-02-12 16:29:17 -0500

    gain_percent

--- finance/common_site.py
+++ finance/common_site.py
@@ -654,7 +654,8 @@ class AgencySiteModel(SiteModel):
     model_order = 4210
     menu_label = MENU_LABEL_FINANCE_ADVANCED
     menu_group = 'Finace Agency'
-    list_display = ('name', 'currency', 'enabled')
+    list_display = ('name', 'currency', 'gain_percent', 'enabled')
+    list_editable = ('gain_percent', 'enabled',)
     top_filters = ('name', 'currency', 'enabled')
     ordering = ['enabled', 'currency', 'name']
 
