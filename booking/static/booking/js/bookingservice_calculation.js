9c5f4a5fb74760ca262b80ea48c81a1374cda41d
Daniel Lodos <ipdlx0@gmail.com>
2019-02-18 16:15:13 -0500

    transfer time

--- booking/static/booking/js/bookingservice_calculation.js
+++ booking/static/booking/js/bookingservice_calculation.js
@@ -46,49 +46,117 @@ $(document).ready(function(){
     return 0
   }
 
-  function compare_time() {
+  function compare_time(evt) {
     // a function to check if computed values differ from set values
     // it highlights different values
     if (timeInput.value == '') {
-      if (isTime(computedTime.html())) {
+      if (is_time(computedTime.html())) {
         timeInput.value = computedTime.html();
         time_autofilled = true;
         $('.btn-copy-time').removeClass('btn-danger');
         $('.btn-copy-time').removeClass('btn-warning');
         $('.btn-copy-time').addClass('btn-success');
-        }
-    } else {
-      if (time_autofilled) {
-        if (isTime(computedTime.html())) {
+        return 0;
+      }
+      time_autofilled = false;
+      $('.btn-copy-time').removeClass('btn-danger');
+      $('.btn-copy-time').removeClass('btn-warning');
+      $('.btn-copy-time').removeClass('btn-success');
+      return 0;
+    }
+    if (time_autofilled) {
+      if (evt.target.id != 'id_time') {
+        if (is_time(computedTime.html())) {
           timeInput.value = computedTime.html();
           $('.btn-copy-time').removeClass('btn-danger');
           $('.btn-copy-time').removeClass('btn-warning');
           $('.btn-copy-time').addClass('btn-success');
-        } else {
-          timeInput.value = '';
-          $('.btn-copy-time').removeClass('btn-danger');
-          $('.btn-copy-time').removeClass('btn-warning');
-          $('.btn-copy-time').removeClass('btn-success');
+          return 0;
         }
-      } else {
-        if (isTime(computedTime.html())) {
-          if (seconds(timeInput.value) != seconds(computedTime.html())){
-            $('.btn-copy-time').removeClass('btn-success');
-            $('.btn-copy-time').removeClass('btn-warning');
-            $('.btn-copy-time').addClass('btn-danger');
-          } else{
+        timeInput.value = '';
+        $('.btn-copy-time').removeClass('btn-danger');
+        $('.btn-copy-time').removeClass('btn-warning');
+        $('.btn-copy-time').removeClass('btn-success');
+        return 0;
+      }
+      time_autofilled = false;
+    }
+    return compare_time_values();
+  }
+    
+  function compare_time_values() {
+    if (is_time(timeInput.value) && is_time(computedTime.html())) {
+      iseconds = seconds(timeInput.value);
+      cseconds = seconds(computedTime.html());
+      if (iseconds == cseconds){
+        $('.btn-copy-time').removeClass('btn-danger');
+        $('.btn-copy-time').removeClass('btn-warning');
+        $('.btn-copy-time').addClass('btn-success');
+        return 0;
+      }
+      if (iseconds < cseconds) {
+          if (cseconds - iseconds < 6 * 3600) {
             $('.btn-copy-time').removeClass('btn-danger');
-            $('.btn-copy-time').removeClass('btn-warning');
-            $('.btn-copy-time').addClass('btn-success');
+            $('.btn-copy-time').removeClass('btn-success');
+            $('.btn-copy-time').addClass('btn-warning');
+            return 0;
           }
-        } else {
-          $('.btn-copy-time').removeClass('btn-danger');
-          $('.btn-copy-time').removeClass('btn-warning');
           $('.btn-copy-time').removeClass('btn-success');
-        }
+          $('.btn-copy-time').removeClass('btn-warning');
+          $('.btn-copy-time').addClass('btn-danger');
+          return 0;
+      }
+      if (iseconds - cseconds > 18 * 3600) {
+        $('.btn-copy-time').removeClass('btn-danger');
+        $('.btn-copy-time').removeClass('btn-success');
+        $('.btn-copy-time').addClass('btn-warning');
+        return 0;
+      }
+      $('.btn-copy-time').removeClass('btn-success');
+      $('.btn-copy-time').removeClass('btn-warning');
+      $('.btn-copy-time').addClass('btn-danger');
+      return 0;
+    }
+    $('.btn-copy-time').removeClass('btn-danger');
+    $('.btn-copy-time').removeClass('btn-warning');
+    $('.btn-copy-time').removeClass('btn-success');
+    return 0;
+  }
+
+  function is_time(str){
+    if (str == '') {
+      return true
+    }
+    var parts = str.split(':');
+    for (let index = 0; index < parts.length; index++) {
+      if (parts[index] && isNaN(parts[index])) {
+        return false;
       }
     }
-    return 0
+    return true;
+  }
+
+  function seconds(str){
+    if (str == '') {
+      return 0
+    }
+    var parts = str.split(':');
+    result = 0;
+    for (let index = 0; index < parts.length; index++) {
+      n = Number(parts[index]);
+      m = 1;
+      if (index == 0) {
+        m = 3600;
+      } else if (index == 1) {
+        m = 60;
+      }
+      if (isNaN(n)) {
+        return 0;
+      } else {
+        result += m * n; 
+      }
+    }
+    return result;
   }
 
   function get_computed_amounts(url, form_dict){
@@ -121,7 +189,7 @@ $(document).ready(function(){
       compare_numbers();
     })
   }
-  function get_computed_time(url, form_dict){
+  function get_computed_time(url, form_dict, evt){
     computedTime.html('Loading...');
     // sending a request to get computed value
     $.ajax({
@@ -136,10 +204,10 @@ $(document).ready(function(){
       } else {
         computedTime.html(data['time_message']);
       }
-      compare_time();
+      compare_time(evt);
     }).fail(function(){
       computedTime.html('N/A');
-      compare_time();
+      compare_time(evt);
     })
   }
 
@@ -158,10 +226,10 @@ $(document).ready(function(){
     get_computed_amounts(bookingservice_amounts_url, data);
   });
 
-  $('#bookingtransfer_form input, #bookingtransfer_form select').on('change', function(){
+  $('#bookingtransfer_form input, #bookingtransfer_form select').on('change', function(evt){
     data = $('#bookingtransfer_form').serialize();
     get_computed_amounts(bookingservice_amounts_url, data);
-    get_computed_time(bookingtransfer_time_url, data);
+    get_computed_time(bookingtransfer_time_url, data, evt);
   });
 
   $('#bookingextra_form input, #bookingextra_form select').on('change', function(){
