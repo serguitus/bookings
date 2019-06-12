ce3c5b97dcca8b239e85fabc5abf970df34a38b0
Daniel Lodos <ipdlx0@gmail.com>
2019-06-12 03:56:45 -0400

    better match navigation

--- common/templatetags/common_utils.py
+++ common/templatetags/common_utils.py
@@ -8,7 +8,7 @@ from django.contrib.admin.templatetags.admin_list import (
 from django.contrib.admin.utils import (
     display_for_field, display_for_value, get_fields_from_path,
     label_for_field, lookup_field)
-from django.contrib.admin.views.main import SEARCH_VAR
+from django.contrib.admin.views.main import ALL_VAR, SEARCH_VAR
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 from django.template.context import Context
@@ -23,9 +23,58 @@ from django.utils.six.moves.urllib.parse import parse_qsl, urlparse, urlunparse
 
 register = template.Library()
 
+DOT = '.'
+
 @register.assignment_tag
 def setvar(val=None):
     return val
+
+@register.inclusion_tag('common/pagination.html')
+def common_pagination(cl):
+    """
+    Generates the series of links to the pages in a paginated list.
+    """
+    paginator, page_num = cl.paginator, cl.page_num
+
+    pagination_required = (not cl.show_all or not cl.can_show_all) and cl.multi_page
+    if not pagination_required:
+        page_range = []
+    else:
+        ON_EACH_SIDE = 3
+        ON_ENDS = 2
+
+        # If there are 10 or fewer pages, display links to every page.
+        # Otherwise, do some fancy
+        if paginator.num_pages <= 10:
+            page_range = range(paginator.num_pages)
+        else:
+            # Insert "smart" pagination links, so that there are always ON_ENDS
+            # links at either end of the list of pages, and there are always
+            # ON_EACH_SIDE links at either end of the "current page" link.
+            page_range = []
+            if page_num > (ON_EACH_SIDE + ON_ENDS):
+                page_range.extend(range(0, ON_ENDS))
+                page_range.append(DOT)
+                page_range.extend(range(page_num - ON_EACH_SIDE, page_num + 1))
+            else:
+                page_range.extend(range(0, page_num + 1))
+            if page_num < (paginator.num_pages - ON_EACH_SIDE - ON_ENDS - 1):
+                page_range.extend(range(page_num + 1, page_num + ON_EACH_SIDE + 1))
+                page_range.append(DOT)
+                page_range.extend(range(paginator.num_pages - ON_ENDS, paginator.num_pages))
+            else:
+                page_range.extend(range(page_num + 1, paginator.num_pages))
+
+    need_show_all_link = cl.can_show_all and not cl.show_all and cl.multi_page
+    return {
+        'cl': cl,
+        'pagination_required': pagination_required,
+        'show_all_url': need_show_all_link and cl.get_query_string({ALL_VAR: ''}),
+        'page_range': page_range,
+        'ALL_VAR': ALL_VAR,
+        '1': 1,
+    }
+
 
 @register.simple_tag(takes_context=True)
 def common_add_preserved_filters(context, url, popup=False, to_field=None):
