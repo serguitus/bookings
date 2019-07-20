fb8fb3aa2c68a91ad3dcc950833c9a546cb117aa
Daniel Lodos <ipdlx0@gmail.com>
2019-07-20 08:33:56 -0400

    booking transfers time

--- booking/static/booking/js/bookingpackagetransfer.js
+++ booking/static/booking/js/bookingpackagetransfer.js
@@ -1,2 +1,4 @@
 var bookingservice_amounts_url = base_url + 'booking/bookingpackagetransfer-amounts/';
 var bookingservice_form_selector = '#bookingpackagetransfer_form';
+
+var bookingservicetransfer_time_url = base_url + 'booking/bookingtransfer-time/';
