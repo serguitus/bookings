51140b203fee6d8791ee8c945179c2b8ef5346f0
serguitus <serguitus@gmail.com>
2019-08-11 23:56:46 -0400

    adding some style to price-export config form

--- config/templatetags/export_prices.py
+++ config/templatetags/export_prices.py
@@ -10,3 +10,8 @@ register = template.Library()
 def service_prices(service=None, agency=None, date_from=None, date_to=None):
     details = ConfigServices.list_service_prices(service, agency, date_from, date_to)
     return {'details': details}
+
+# a filter to add css classes to widgets. used to style form fields
+@register.filter(name='addclass')
+def addclass(value, arg):
+    return value.as_widget(attrs={'class': arg})
