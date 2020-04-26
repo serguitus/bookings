0726d993a1df6faf647f778b315ad5a0f4d21782
Daniel Lodos <ipdlx0@gmail.com>
2020-04-26 09:54:11 -0400

    provider and agency service details as tables instead of inlines

--- config/templatetags/config_tags.py
+++ config/templatetags/config_tags.py
@@ -1,6 +1,12 @@
 from __future__ import unicode_literals
-from django import template
 
+from django import template
+from django.forms.models import modelformset_factory
+
+from config.models import (
+    ProviderAllotmentDetail, ProviderTransferDetail, ProviderExtraDetail,
+    AgencyAllotmentDetail, AgencyTransferDetail, AgencyExtraDetail,
+)
 from config.tables import (
     ServiceBookDetailTable,
     ProviderAllotmentDetailTable, ProviderTransferDetailTable, ProviderExtraDetailTable,
@@ -17,49 +23,49 @@ def servicebookdetail_table(service):
     return table
 
 @register.simple_tag
-def providerallotmentdetail_table(providerservice):
+def providerallotmentdetail_context(providerservice):
     table = ProviderAllotmentDetailTable(
         providerservice.providerallotmentdetail_set.all() \
             .select_related('room_type', 'addon'),
         order_by=('room_type', 'board_type'))
-    return table
+    return {'table': table}
 
 @register.simple_tag
-def providertransferdetail_table(providerservice):
+def providertransferdetail_context(providerservice):
     table = ProviderTransferDetailTable(
         providerservice.providertransferdetail_set.all() \
             .select_related('location_from', 'location_to', 'addon'),
         order_by=('location_from', 'location_to'))
-    return table
+    return {'table': table}
 
 @register.simple_tag
-def providerextradetail_table(providerservice):
+def providerextradetail_context(providerservice):
     table = ProviderExtraDetailTable(
         providerservice.providerextradetail_set.all() \
             .select_related('addon'),
         order_by=('addoin', 'pax_range_min', 'pax_range_min'))
-    return table
+    return {'table': table}
 
 @register.simple_tag
-def agencyallotmentdetail_table(agencyservice):
+def agencyallotmentdetail_context(agencyservice):
     table = AgencyAllotmentDetailTable(
         agencyservice.agencyallotmentdetail_set.all() \
             .select_related('room_type', 'addon'),
         order_by=('room_type', 'board_type'))
-    return table
+    return {'table': table}
 
 @register.simple_tag
-def agencytransferdetail_table(agencyservice):
+def agencytransferdetail_context(agencyservice):
     table = AgencyTransferDetailTable(
         agencyservice.agencytransferdetail_set.all() \
             .select_related('location_from', 'location_to', 'addon'),
         order_by=('location_from', 'location_to'))
-    return table
+    return {'table': table}
 
 @register.simple_tag
-def agencyextradetail_table(agencyservice):
+def agencyextradetail_context(agencyservice):
     table = AgencyExtraDetailTable(
         agencyservice.agencyextradetail_set.all() \
             .select_related('addon'),
         order_by=('addoin', 'pax_range_min', 'pax_range_min'))
-    return table
+    return {'table': table}
