40cb175de0d325ffee3cce835fe6e32496cfffe1
serguitus <serguitus@gmail.com>
2020-06-16 17:16:35 -0400

    removing useless lines after refactoring details addition UI

--- config/static/config/js/add_service_detail.js
+++ config/static/config/js/add_service_detail.js
@@ -1,8 +1,3 @@
-// function showSearchServiceModal(parent_id) {
-//   $('#id_parent_id').val(parent_id)
-//   $('#searchServiceModal').modal('show');
-// }
-
 $(document).ready(function(){
 
   $('#searchServiceModal').on('show.bs.modal', function (event) {
@@ -12,27 +7,6 @@ $(document).ready(function(){
   form_obj.attr('action', add_service_url)
   $('#id_parent_id').val(parent_id)
   });
-
-  // $('#add_service_dialog_btn').click(function() {
-  //   // sending a request to get service detail url
-
-  //   $.ajax({
-  //     'url': add_service_url,
-  //     'async': true,
-  //     'datatype': 'json',
-  //     'type': 'POST',
-  //     'data': $('form.search_service').serialize(),
-  //   }).done(function(data){
-  //     if (data.url) {
-  //       window.location.href = base_url + data.url;
-  //     } else {
-  //       console.error('ajax add service url returned with no data');
-  //     }
-  //   }).fail(function() {
-  //     console.error('ajax add service url failed');
-  //   })
-  // });
-
 
 });
 
