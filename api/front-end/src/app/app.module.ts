67fe9e0d8d70043e423a01c1c10593a25153e2ee
serguitus <serguitus@gmail.com>
2020-06-24 13:50:15 -0400

    adding built files and user.service.ts

--- api/front-end/src/app/app.module.ts
+++ api/front-end/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { UserService } from './user.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule, HttpClientModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
